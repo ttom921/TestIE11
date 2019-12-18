fded47e85379123a766dbb829b1fe00a10c61936
ttom921 <ttom921@hotmail.com>
2019-12-18 10:38:49 +0800

    第一版可以form傳給web

--- ie11/src/app/app.component.ts
+++ ie11/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ie11';
+  constructor(
+    private ngZone: NgZone,
+  ) {
+
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    window['angularComponentReference'] = { component: this, zone: this.ngZone, loadAngularFunction: (arg1, arg2) => this.angularFunctionCalled(arg1, arg2), };
+  }
+  public angularFunctionCalled(arg1, arg2) {
+    let alertMessage = "angular :arg1 is " + arg1 + " and arg2 is " + arg2 + "!";
+    alert(alertMessage);
+    //console.log("arg1 is " + arg1 + " and arg2 is " + arg2 + "!");
+    //"arg1 is " + arg1 + " and arg2 is " + arg2 + "!"
+  }
 }
+
---
+++ testMRWebBrowser/testMRWebBrowser/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Windows.Forms;
+
+namespace testMRWebBrowser
+{
+
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [ComVisible(true)]
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //string applicationDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            //string myFile = Path.Combine(applicationDirectory, "S_RCDATA\\index.html");
+
+            webBrowser1.ScriptErrorsSuppressed = true;
+            //http://localhost:4400/ 
+            //http://172.18.2.160/
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            //object ret = mrWebBrowser.SendToJS("ShowAlert", new object[] { "XXXXXXXXXXXXXXXAAAA" });
+            object[] args = { "argString1", "argString2" };
+            object ret = webBrowser1.Document.InvokeScript("callMe", args);
+        }
+
+        public void OnWebPageReady(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+    }
+}
